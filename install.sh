2d8b1ffef49c7eea09c9f7f635162cd50cc94f2f
Li-Wei Chen <wayne900619@gmail.com>
2025-04-14 14:46:19 +0800

    feat: update  neovim download url

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ applynvim() {
 		# echo "nvim is not installed."
 		# return $(false)
 		mkdir -p $HOME/.local/bin
-		curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz | tar zxf - -C $HOME
+		curl -fsSL https://github.com/neovim/neovim/releases/download/stable/nvim-linux-x86_64.tar.gz | tar zxf - -C $HOME
 		ln -s $HOME/nvim-linux64/bin/nvim $HOME/.local/bin/nvim
 	fi
 
